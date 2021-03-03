c5bfb1b1d20a69f50481cf035346cb6122eaa7d7
Douglas Cutz <douglascutz@mesoamericana.edu.gt>
2021-03-03 17:14:20 -0600

    Se crearon las clases de empleado y asistencia

---
+++ L1 de REPASO/Asistencia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1_de_REPASO
+{
+    class Asistencia
+    {
+        int codigo;
+        int horaMes;
+        string mes;
+
+    }
+}
---
+++ L1 de REPASO/Empleados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1_de_REPASO
+{
+    class Empleados
+    {
+        int codigo;
+        string nombre;
+        float sueldoHora;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public float SueldoHora { get => sueldoHora; set => sueldoHora = value; }
+    }
+}
