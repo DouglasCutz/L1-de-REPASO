31ad009599093b19776f2bf75ad2ab5e44de3499
Douglas Cutz <douglascutz@mesoamericana.edu.gt>
2021-03-04 15:40:25 -0600

    Completado primera Fase

--- L1 de REPASO/Form1.Designer.cs
+++ L1 de REPASO/Form1.Designer.cs
@@ -35,20 +35,20 @@ namespace L1_de_REPASO
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tx_codigo = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tx_nombre = new System.Windows.Forms.TextBox();
+            this.tx_sueldohora = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tx_codi2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.tx_horasmes = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.tx_mes = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btn_ingreso1 = new System.Windows.Forms.Button();
+            this.Ingreso2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
@@ -109,12 +109,12 @@ namespace L1_de_REPASO
             this.label1.TabIndex = 6;
             this.label1.Text = "No. Empleado";
             // 
-            // textBox1
+            // tx_codigo
             // 
-            this.textBox1.Location = new System.Drawing.Point(766, 36);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(246, 22);
-            this.textBox1.TabIndex = 7;
+            this.tx_codigo.Location = new System.Drawing.Point(766, 36);
+            this.tx_codigo.Name = "tx_codigo";
+            this.tx_codigo.Size = new System.Drawing.Size(246, 22);
+            this.tx_codigo.TabIndex = 7;
             // 
             // label2
             // 
@@ -125,19 +125,19 @@ namespace L1_de_REPASO
             this.label2.TabIndex = 8;
             this.label2.Text = "Nombre:";
             // 
-            // textBox2
+            // tx_nombre
             // 
-            this.textBox2.Location = new System.Drawing.Point(766, 76);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(246, 22);
-            this.textBox2.TabIndex = 9;
+            this.tx_nombre.Location = new System.Drawing.Point(766, 76);
+            this.tx_nombre.Name = "tx_nombre";
+            this.tx_nombre.Size = new System.Drawing.Size(246, 22);
+            this.tx_nombre.TabIndex = 9;
             // 
-            // textBox3
+            // tx_sueldohora
             // 
-            this.textBox3.Location = new System.Drawing.Point(872, 115);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(140, 22);
-            this.textBox3.TabIndex = 11;
+            this.tx_sueldohora.Location = new System.Drawing.Point(872, 115);
+            this.tx_sueldohora.Name = "tx_sueldohora";
+            this.tx_sueldohora.Size = new System.Drawing.Size(140, 22);
+            this.tx_sueldohora.TabIndex = 11;
             // 
             // label3
             // 
@@ -148,28 +148,28 @@ namespace L1_de_REPASO
             this.label3.TabIndex = 10;
             this.label3.Text = "Sueldo por hora:";
             // 
-            // textBox4
+            // tx_codi2
             // 
-            this.textBox4.Location = new System.Drawing.Point(786, 265);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(226, 22);
-            this.textBox4.TabIndex = 13;
+            this.tx_codi2.Location = new System.Drawing.Point(786, 265);
+            this.tx_codi2.Name = "tx_codi2";
+            this.tx_codi2.Size = new System.Drawing.Size(226, 22);
+            this.tx_codi2.TabIndex = 13;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(663, 265);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.Size = new System.Drawing.Size(101, 17);
             this.label4.TabIndex = 12;
-            this.label4.Text = "No. de Empleado:";
+            this.label4.Text = "No. Empleado:";
             // 
-            // textBox5
+            // tx_horasmes
             // 
-            this.textBox5.Location = new System.Drawing.Point(786, 304);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(226, 22);
-            this.textBox5.TabIndex = 15;
+            this.tx_horasmes.Location = new System.Drawing.Point(786, 304);
+            this.tx_horasmes.Name = "tx_horasmes";
+            this.tx_horasmes.Size = new System.Drawing.Size(226, 22);
+            this.tx_horasmes.TabIndex = 15;
             // 
             // label5
             // 
@@ -180,12 +180,12 @@ namespace L1_de_REPASO
             this.label5.TabIndex = 14;
             this.label5.Text = "Horas del mes:";
             // 
-            // textBox6
+            // tx_mes
             // 
-            this.textBox6.Location = new System.Drawing.Point(786, 347);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(226, 22);
-            this.textBox6.TabIndex = 17;
+            this.tx_mes.Location = new System.Drawing.Point(786, 347);
+            this.tx_mes.Name = "tx_mes";
+            this.tx_mes.Size = new System.Drawing.Size(226, 22);
+            this.tx_mes.TabIndex = 17;
             // 
             // label6
             // 
@@ -206,43 +206,44 @@ namespace L1_de_REPASO
             this.dataGridView3.Size = new System.Drawing.Size(539, 176);
             this.dataGridView3.TabIndex = 18;
             // 
-            // button3
+            // btn_ingreso1
             // 
-            this.button3.Location = new System.Drawing.Point(822, 177);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(111, 31);
-            this.button3.TabIndex = 19;
-            this.button3.Text = "Ingresar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btn_ingreso1.Location = new System.Drawing.Point(822, 177);
+            this.btn_ingreso1.Name = "btn_ingreso1";
+            this.btn_ingreso1.Size = new System.Drawing.Size(111, 31);
+            this.btn_ingreso1.TabIndex = 19;
+            this.btn_ingreso1.Text = "Ingresar";
+            this.btn_ingreso1.UseVisualStyleBackColor = true;
+            this.btn_ingreso1.Click += new System.EventHandler(this.btn_ingreso1_Click);
             // 
-            // button4
+            // Ingreso2
             // 
-            this.button4.Location = new System.Drawing.Point(822, 406);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(111, 31);
-            this.button4.TabIndex = 20;
-            this.button4.Text = "Ingresar";
-            this.button4.UseVisualStyleBackColor = true;
+            this.Ingreso2.Location = new System.Drawing.Point(822, 406);
+            this.Ingreso2.Name = "Ingreso2";
+            this.Ingreso2.Size = new System.Drawing.Size(111, 31);
+            this.Ingreso2.TabIndex = 20;
+            this.Ingreso2.Text = "Ingresar";
+            this.Ingreso2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1104, 773);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Ingreso2);
+            this.Controls.Add(this.btn_ingreso1);
             this.Controls.Add(this.dataGridView3);
-            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.tx_mes);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.tx_horasmes);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tx_codi2);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.tx_sueldohora);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tx_nombre);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tx_codigo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -267,20 +268,20 @@ namespace L1_de_REPASO
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tx_codigo;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox tx_nombre;
+        private System.Windows.Forms.TextBox tx_sueldohora;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox tx_codi2;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox tx_horasmes;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox tx_mes;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.DataGridView dataGridView3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn_ingreso1;
+        private System.Windows.Forms.Button Ingreso2;
     }
 }
 
--- L1 de REPASO/Form1.cs
+++ L1 de REPASO/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,64 @@ using System.Windows.Forms;
 
 namespace L1_de_REPASO
 {
+    
     public partial class Form1 : Form
     {
+        List<Empleados> empleados = new List<Empleados>();
         public Form1()
         {
             InitializeComponent();
+        }
+        void Limpiar1()
+        {
+            tx_codigo.Text="";
+            tx_nombre.Text="";
+            tx_sueldohora.Text = "";
+        }
+        void Mostrar1()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = empleados;
+            dataGridView1.Refresh();
+        }
+        private void Guardar1(String archivo, String codi, String nom , String sal )
+        {
+            //Abrir el archivo: Write sobreescribe el archivo, Append agrega los datos al final del archivo
+            FileStream stream = new FileStream(archivo, FileMode.Append, FileAccess.Write);
+            //Crear un objeto para escribir el archivo
+            StreamWriter writer = new StreamWriter(stream);
+            //Usar el objeto para escribir al archivo, WriteLine, escribe linea por linea
+            //Write escribe todo en la misma linea. En este ejemplo se hará un dato por cada línea
+            //writer.WriteLine(texto);
+
+             writer.WriteLine(codi);
+             writer.WriteLine(nom);
+             writer.WriteLine(sal);
+            
+            //Cerrar el archivo
+            writer.Close();
+        }
+
+
+        private void btn_ingreso1_Click(object sender, EventArgs e)
+        {
+            Empleados empleado = new Empleados();
+
+            if ((tx_codigo.Text != "") && (tx_nombre.Text != "") && (tx_sueldohora.Text != ""))
+            {
+                empleado.Codigo = Convert.ToInt16(tx_codigo.Text);
+                empleado.Nombre = tx_nombre.Text;
+                empleado.SueldoHora = System.Convert.ToSingle(tx_sueldohora.Text);
+                empleados.Add(empleado);
+               
+                MessageBox.Show(" Agregado ");
+                Mostrar1();
+                Guardar1("Empleados.txt",tx_codigo.Text, tx_nombre.Text, tx_sueldohora.Text);
+                Limpiar1();
+
+            }
+            else
+                MessageBox.Show(" Por favor llene todos los campos");
         }
     }
 }
